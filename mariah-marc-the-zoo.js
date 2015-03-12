ca275bb80ff61bf29f8a7f7372a4b47a99697938
Marc Nischan <marcnischan@gmail.com>
2015-03-12 15:30:23 -0400

    Adding the Animal and Pan constructor objects

---
+++ mariah-marc-the-zoo.js
@@ -0,0 +1,24 @@
+
+//1) Create a new Animal
+//2) Set up new Pens
+//3) Remove Pens
+//4) Add an Animal to a Pen
+//5) Remove Animals from pens (might be cool as a 2.0 feature to have this feature be add/remove and you type the animal... if it exists then delete it -maybe with a confirm- and if it doesn't exist, create it
+//6) Display all the Animals in a Pen
+//7) Display all the Animals in the Zoo
+
+//SET UP PROTOTYPE OBJECTS
+//Create new Animals (animals should have a "species," "size," and "gender")
+
+function Animal(species, size, gender){
+	this.species = species;
+	this.size = size;
+	this.gender = gender;
+};
+// Create a pen
+function Pen(){
+	this.habitat = habitat;
+	this.population = population;
+	this.momAndDad = momAndDad;
+	this.ocupant = occupant;
+};
