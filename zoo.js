09bf32d6a4c54205622d4c9b62b4b305177e797e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-03-12 16:46:32 -0400

    Added basic menu and user input.

--- zoo.js
+++ zoo.js
@@ -1,3 +1,5 @@
+var sget = require('sget');
+
 var Animal = function(name, species, size, gender){
 	this.name = name;
 	this.species = species;
@@ -6,18 +8,39 @@ var Animal = function(name, species, size, gender){
 }
 
 var Zoo = {
+	
 	pens: [],
 
-	addPen: function(pen){
+	addPen: function(){
 
 	},
 
-	deletePen: function(pen){
+	deletePen: function(){
 
 	},
 
 	displayZoo: function(){
 
+	},
+
+	handleInput: function(){
+		var userInput = sget("Please enter your choice: ").trim();
+		if (isNaN(userInput)){
+			console.log("Invalid selection, please try again.")
+			return this.handleInput();
+		} else
+			return userInput;
+	},
+
+	zooMenu: function(){
+		
+		console.log("Welcome to the Zoo\n===========================\nYou are our new Zookeeper\nHere are your choices:\n1) Create a new pen for your animals.\n2) Remove a pen from the zoo.\n3) Add an animal to a pen.\n4) Remove an animal from a pen.\n5) Display all zoo animals.\n6) Display animals in a specific pen.");
+		this.handleInput();
+	
+	},
+
+	testing: function(){
+		console.log("ttttttttt");
 	}
 
 } 
@@ -43,14 +66,19 @@ var Pen = function(type, animals){
 }
 
 
+
+var tempPen = new Pen("Animal Storage", [monkey, snake]);
+
 var lion1 = new Animal('Bob', 'Lion', 'large', 'male');
 var lion2 = new Animal('Bill', 'Lion', 'large', 'male');
-var lion3 = new Animal('Bill', 'Lion', 'large', 'male');
+var lion3 = new Animal('Fred', 'Lion', 'large', 'male');
 var monkey = new Animal('Sara', 'Monkey', 'medium', 'female');
 var snake = new Animal('Sam', 'Snake', 'small', 'male');
 
 var lionPen = new Pen("Lion's Den",[lion1, lion2, lion3]);
 
-
+// var zoo = new Zoo();
 
 console.log(lionPen);
+
+Zoo.zooMenu();
