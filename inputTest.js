40294efc2a66e56b7691315e5b013b3b94463288
Tom Joseph <tom.joseph@detroitlabs.com>
2015-03-12 16:00:08 -0400

    fixing bugs

--- inputTest.js
+++ inputTest.js
@@ -1,22 +1,27 @@
 var sget = require('sget');
 
 function getInput(message, verification) {
-	var varified = false;
+	var verified = false;
 	var input = undefined; 
 
-	while(!varified) {
+	while(!verified) {
 		input = sget(message).trim().toLowerCase(); 
+		console.log("Test");
 		if (verification !== undefined) {
+			console.log("needs to verify");
 			verified = verifyInput(input, verification);
+			console.log(")))" + verified);
 		} else {
 			verified = true;
 		}
 	}
+	console.log ("LOOP ENDED");
 	return input;
 }
 
 function verifyInput(input, verification) {
 	if (verification.indexOf(input) !== -1) {
+		console.log("will return true");
 		return true;
 	} else {
 		console.log("Invalid entry, please try again");
