848ecb5abe02732c7f1fa8bca8a2819b03fc90ed
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-03-12 16:00:27 -0400

    adding animal and pen constructors

--- zoo.js
+++ zoo.js
@@ -1,1 +1,26 @@
-console.log("Howday!");
+var sget = require("sget");
+
+function getUserInput(message) {
+	return sget(message).trim();
+}
+
+function Animal (name, species, size, gender){
+	this.name = name;
+	this.species = species;
+	this.size = size;
+	this.gender = gender;
+};
+
+function Pen (){
+	var animals = [];
+
+	this.addAnimal = function (name, species, size, gender) {
+		console.log("Adding %s to this pen.", name);
+		this.entries.push(new Animal(name, species, size, gender));
+	};
+	// call this method like this this.addEntry(getUserInput("Please enter a Name "), getUserInput("Phone Number "), getUserInput("Address"));
+};
+
+var penOne = new Pen();
+
+penOne.addAnimal(getUserInput("Please enter a Name "), getUserInput("species "), getUserInput("size"), getUserInput("gender"));
