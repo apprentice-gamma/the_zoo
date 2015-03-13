0fd0f6d2e173d8e88d392c1f677dd14a86c2a2c5
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-03-13 11:18:21 -0400

    adding display methods

--- zoo.js
+++ zoo.js
@@ -9,18 +9,45 @@ function Animal (name, species, size, gender){
 	this.species = species;
 	this.size = size;
 	this.gender = gender;
+
+	this.displayName = function(){
+    	console.log(this.name);
+	};
+
+	this.displayAnimalInfo = function(){
+    	console.log("Animal Information\n  name:%s\n  species:%s\n  size:%s\n  gender:%s\n", this.name, this.species, this.size, this.gender);
+	};
 };
 
 function Pen (){
-	var animals = [];
+	this.animals = [];
 
 	this.addAnimal = function (name, species, size, gender) {
 		console.log("Adding %s to this pen.", name);
-		this.entries.push(new Animal(name, species, size, gender));
+		this.animals.push(new Animal(name, species, size, gender));
 	};
 	// call this method like this this.addEntry(getUserInput("Please enter a Name "), getUserInput("Phone Number "), getUserInput("Address"));
+
+	this.listAnimals = function(){
+		for (var i = 0; i < this.animals.length; i++) {
+			this.animals[i].displayName();
+		};
+	};
+};
+
+function Zoo (){
+	var pens = []
 };
 
 var penOne = new Pen();
 
 penOne.addAnimal(getUserInput("Please enter a Name "), getUserInput("species "), getUserInput("size"), getUserInput("gender"));
+
+penOne.listAnimals();
+
+
+
+
+
+
+
