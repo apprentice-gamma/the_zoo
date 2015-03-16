d324a51719cbe66bca230e63835f35a728f0c82c
Greg Santikian <gsantikian@gmail.com>
2015-03-16 15:43:09 -0400

    Lots of changes

--- zoo.js
+++ zoo.js
@@ -1,7 +1,7 @@
 var sget = require("sget");
 
 function Zoo() {
-  this.animals = [];
+  // this.animals = [];
   this.pens = [];
 
   this.addAnimal = function(animal) {
@@ -18,8 +18,8 @@ function Zoo() {
     }
   };
 
-  this.addAnimalToPen = function(animalName, penName) {
-    this.findPenByName(penName).animals.push(this.findAnimalByName(animalName));
+  this.removePen = function(pen) {
+    this.pens.splice(this.pens.indexOf(pen), 1);
   };
 
   this.findPenByName = function(penName) {
@@ -40,7 +40,7 @@ function Zoo() {
 
   this.showAllAnimals = function() {
     this.animals.forEach(function(animal) {
-      console.log("Name: " + animal.name + "\tSpecies: " + animal.species + "\tSize: " + animal.size + "\tGender: " + animal.gender + "\n");
+      console.log("Name: " + animal.name + "\tSpecies: " + animal.species + "\tSize: " + animal.size + "\tGender: " + animal.gender + "\tLocation: " + animal.location + "\n");
     });
   };
 }
@@ -50,9 +50,11 @@ function Pen(name) {
   this.animals = [];
 }
 
+
+
 Pen.prototype.displayAnimals = function() {
   this.animals.forEach(function(animal) {
-    console.log("Name: " + animal.name + "\tSpecies: " + animal.species + "\tSize: " + animal.size + "\tGender: " + animal.gender + "\n");
+    console.log("Name: " + animal.name + "\tSpecies: " + animal.species + "\tSize: " + animal.size + "\tGender: " + animal.gender + "\tLocation: " + animal.location + "\n");
   });
 };
 
@@ -60,8 +62,14 @@ Pen.prototype.removeAnimal = function(animalName) {
   for (var i = 0, len = this.animals.length; i < len; i++) {
     if (this.animals[i].name === animalName) {
       this.animals.splice(i, 1);
+    } else {
+      console.log("That animal isn't in this pen.");
     }
   }
+};
+
+Pen.prototype.addAnimal = function(animal) {
+  this.animals.push(animal);
 };
 
 function Animal(name, species, size, gender) {
@@ -90,13 +98,15 @@ zoo.addPen(butterflyHouse);
 // zoo.listPens();
 zoo.showAllAnimals();
 
-console.log("Dogpound pre-addition: " + dogPound.animals.length);
-console.log("Zoo animals array pre " + zoo.animals.length);
-zoo.addAnimalToPen("spuds", "Dog pound");
-console.log("Dogpound post-addition: " + dogPound.animals.length);
-console.log("Zoo animals array post " + zoo.animals.length);
+// console.log("Dogpound pre-addition: " + dogPound.animals.length);
+// console.log("Zoo animals array pre " + zoo.animals.length);
+// zoo.addAnimalToPen("spuds", "Dog pound");
+// console.log("Dogpound post-addition: " + dogPound.animals.length);
+// console.log("Zoo animals array post " + zoo.animals.length);
 
+zoo.findPenByName("Dog pound").displayAnimals();
 console.log("NEW");
+zoo.findPenByName("Dog pound").removeAnimal("spuds");
 zoo.findPenByName("Dog pound").displayAnimals();
 // var bessie = zoo.findAnimalByName("bessie");
 // console.log(bessie.name);
@@ -106,55 +116,54 @@ zoo.findPenByName("Dog pound").displayAnimals();
 // console.log(pound.name);
 // console.log(pound.animals);
 
-// var zooKeeping = true;
+var zooKeeping = true;
 
-// console.log("Welcome to the zoo!");
+console.log("Welcome to the zoo!");
 
-// while (zooKeeping) {
-// 	console.log("What would you like to do in the zoo?\n"
-// 		+ "1: Add an animal.\n"
-// 		+ "2: Build an animal pen.\n"
-// 		+ "3: Remove a pen.\n"
-// 		+ "4: Add an animal to a pen.\n"
-// 		+ "5: Remove an animal from a pen.\n"
-// 		+ "6: View all the animals in a pen.\n"
-// 		+ "7: View all the animals in the zoo.\n"
-// 		+ "8: Quit");
-// 	var userInput = getInput();
+while (zooKeeping) {
+	console.log("What would you like to do in the zoo?\n"
+		+ "1: Add an animal.\n"
+		+ "2: Build an animal pen.\n"
+		+ "3: Remove a pen.\n"
+		+ "4: Add an animal to a pen.\n"
+		+ "5: Remove an animal from a pen.\n"
+		+ "6: View all the animals in a pen.\n"
+		+ "7: View all the animals in the zoo.\n"
+		+ "8: Quit");
+	var userInput = getInput();
 
-// 	switch (userInput) {
-// 	  case '1':
-// 	    zoo.addAnimal(new Animal(getInput("Enter species:"), getInput("Enter size:"), getInput("Enter gender:")));
-// 	    break;
-
-// 	  case '2':
-// 	    zoo.addPen(new Pen(getInput("Enter name of pen:")));
-// 	    break;
-
-// 	  case '3':
-// 	  	zoo.listPens();
-// 	  	zoo.removePen(getInput("Type the name of the pen you would like to remove:"));
-// 	  	break;
-
-// 	  case '4':
-// 		    break;
-
-// 	  case '5':
-// 	    break;
-
-// 	  case '6':
-// 	    break;
-
-// 	  case '7':
-// 	    break;
-
-// 	  case '8':
-// 	  	console.log("Thank you for visiting the zoo.  Please come again.")
-// 	  	zooKeeping = false;
-// 	    break;
-// 	  default:
-// 	  	console.log("Invalid input. Please select from the options below.");
-// 	  	continue;
-// 	}
-
-// }
+	switch (userInput) {
+	  case '1':
+	    zoo.addAnimal(new Animal(getInput("Enter animal's name:"), getInput("Enter species:"), getInput("Enter size:"), getInput("Enter gender:")));
+	    break;
+	  case '2':
+	    zoo.addPen(new Pen(getInput("Enter name of pen:")));
+	    break;
+	  case '3':
+	  	zoo.listPens();
+      var pen = zoo.findPenByName(getInput("Type the name of the pen you would like to remove:"));
+	  	zoo.removePen(pen);
+	  	break;
+	  case '4':
+      zoo.showAllAnimals();
+      var animal = zoo.findAnimalByName(getInput("Which animal would you like to move?"));
+      zoo.listPens();
+      pen = zoo.findPenByName(getInput("Which pen would you like to move " + animal.name + " to?"));
+      pen.addAnimal(animal);
+      console.log(pen.animals)
+		  break;
+	  case '5':
+	    break;
+	  case '6':
+	    break;
+	  case '7':
+	    break;
+	  case '8':
+	  	console.log("Thank you for visiting the zoo.  Please come again.");
+	  	zooKeeping = false;
+	    break;
+	  default:
+	  	console.log("Invalid input. Please select from the options below.");
+	  	continue;
+	}
+}
